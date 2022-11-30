42e6a2fa5bd36c633439a45627c0ed0f019e5d72
Christian Tschub <christian.tschub@vaf-technik.de>
2022-11-30 10:10:56 +0100

    Initial project setup

---
+++ Application/Fixtures.sql
@@ -0,0 +1,34 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.posts DISABLE TRIGGER ALL;
+
+INSERT INTO public.posts (id, title, body) VALUES ('696af632-a78e-4e69-bc86-1c4e66dcd041', 'Hello World!', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam');
+
+
+ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.schema_migrations DISABLE TRIGGER ALL;
+
+INSERT INTO public.schema_migrations (revision) VALUES (1669722244);
+INSERT INTO public.schema_migrations (revision) VALUES (1669729727);
+INSERT INTO public.schema_migrations (revision) VALUES (1669729785);
+
+
+ALTER TABLE public.schema_migrations ENABLE TRIGGER ALL;
+
+
